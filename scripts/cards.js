98eff2c69c2ab93162f723e13e0ea138139ffc0f
Anna Konkova <yndx.annkonkova@yandex.ru>
2024-10-23 17:18:47 +0300

    fix: changed first parameter name for createCard function, added alt text for card

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       cardName: "Алтай",
       cardLink: "../images/altai.jpg",
+      altText: "Фотография лазурного гейзерное озера в Алтае",
     },
     {
       cardName: "Камчатка",
       cardLink: "../images/kamchatka.jpg",
+      altText: "Фотография дороги к большому вулкану на Камчатке в начале осени",
     },
     {
       cardName: "Мурманск",
       cardLink: "../images/murmansk.jpg",
+      altText: "Фотография красивого пересечения дорог на фоне природы Марманска",
     },
     {
       cardName: "Северная Осетия",
       cardLink: "../images/severnaya-osetia.jpg",
+      altText: "Фотография из небольшого просвета в пещере с видом на красивые снежные горы",
     },
     {
       cardName: "Красная поляна",
       cardLink: "../images/krasnaya-polyana.jpg",
+      altText: "Фотография озера по пути к Бзерпскому карнизу в районе Красной Поляны",
     },
     {
       cardName: "Иркутск",
       cardLink: "../images/irkutsk.jpg",
+      altText: "Фотография замерзшего озера Байкал, где видно покрытый снегом скалистый выступ",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 const cardsContainer = document.querySelector(".places__list");
 
-function createCard(initialCards, deleteCallback) {
+function createCard(cardData, deleteCallback) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__image").src = initialCards.cardLink;
-  cardElement.querySelector(".card__title").textContent = initialCards.cardName;
+  cardElement.querySelector(".card__image").src = cardData.cardLink;
+  cardElement.querySelector(".card__title").textContent = cardData.cardName;
+  cardElement.querySelector(".card__image").alt = cardData.altText;
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
   const deleteFunction = () => deleteCallback(cardElement);
