15cd8d25ef149ac7a985575857c165927fcfc530
Anna Konkova <yndx.annkonkova@yandex.ru>
2024-11-22 16:27:24 +0300

    fix: changed the order of variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './pages/index.css'
-import './scripts/cards.js'
 import { initialCards } from './scripts/cards.js'
 import { createCard, deleteCard, likeCard } from './scripts/card.js'
 import { openModal, closeModal, closePopupOnOverlayClick } from './scripts/modal.js'
@@ -12,24 +11,24 @@ const profilePopupForm = document.forms['edit-profile']
 const profileTitle = document.querySelector('.profile__title')
 const profileDescription = document.querySelector('.profile__description')
 const popupCloseButton = document.querySelectorAll('.popup__close')
-const popupCaption = imagePopupElement.querySelector('.popup__caption')
 const cardCreateButton = document.querySelector('.profile__add-button')
 const cardCreatePopup = document.querySelector('.popup_type_new-card')
 const cardCreateForm = document.forms['new-place']
 const formElement = document.querySelector('.popup__form')
 const nameInput = document.querySelector('.popup__input_type_name')
 const jobInput = document.querySelector('.popup__input_type_description')
-const imagePopup = imagePopupElement.querySelector('.popup__image')
 const imagePopupElement = document.querySelector('.popup_type_image')
+const popupCaption = imagePopupElement.querySelector('.popup__caption')
+const imagePopup = imagePopupElement.querySelector('.popup__image')
 
 //инициализируем все карточки из массива
-initialCards.forEach((card) => {
-  cardsContainer.append(createCard(card, deleteCard, likeCard, openImagePopup))
+initialCards.forEach((item) => {
+  cardsContainer.append(createCard(item, deleteCard, likeCard, openImagePopup))
 })
 
 //добавляем всем поп-апам анимацию для плавного появления
-allPopupsOnPage.forEach((popup) => {
-  popup.classList.add('popup_is-animated')
+allPopupsOnPage.forEach((item) => {
+  item.classList.add('popup_is-animated')
 })
 
 // закрытие по крестику для всех попапов
